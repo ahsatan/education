011e1442a7f04583655155c3df1722dcf89cf636
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 16:17:47 -0700

    1a: P2 evaluation primitives problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P2_evaluation_prims.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P2_evaluation_prims) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write out the step-by-step evaluation for the following expressions:
+
+
+(+ (* 2 3) (/ 8 2))
+
+(* (string-length "foo") 2)
