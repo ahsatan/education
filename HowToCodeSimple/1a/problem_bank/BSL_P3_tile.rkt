578b6e46f83c2858c2681f253400d77a1e446dba
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 16:21:45 -0700

    1a: P3 tile problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P3_tile.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P3_tile) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Use the DrRacket square, beside, and above functions to create an image like this one:
+; 
+; .
+; 
+
+
+(require 2htdp/image)
