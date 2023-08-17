6a2ab7ef7f9681f00ac0d982d68cb9d07d5aecef
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 21:15:26 -0700

    1a: P10 triangle problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P10_triangle.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P10_triangle) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write an expression that uses triangle, overlay, and rotate to produce an image similar to this:
+; 
+;                                   .
+; 
+
+
+
