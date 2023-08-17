1b689e3b914623609626aea85bc3f5055e90936e
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 09:35:28 -0700

    1b: simple example code-along problem

---
+++ HowToCodeSimple/1b/code_along/03_simple_example.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 03_simple_example) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function called yell that consumes strings
+; like "hello" and adds "!" to produce strings like "hello!".
+; 
