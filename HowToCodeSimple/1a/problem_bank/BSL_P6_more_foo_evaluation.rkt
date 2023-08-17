9d71f03df7897e130d14c3fee9028b9d35d44499
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 21:00:19 -0700

    1a: P6 more foo evaluation problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P6_more_foo_evaluation.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P6_more_foo_evaluation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write out the step-by-step evaluation of the expression below.
+; 
+
+
+(define (foo n)
+  (* n n))
+
+(foo (+ 3 4))
