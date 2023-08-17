67989fe6f53688d19d064f44a40f383f0492f89b
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 21:07:14 -0700

    1a: P8 cflag problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P8_cflag.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P8_cflag) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; The background for the Canadian Flag (without the maple leaf) is this:
+;          .
+;          
+; Write an expression to produce that background. (If you want to get the
+; details right, officially the overall flag has proportions 1:2, and the 
+; band widths are in the ratio 1:2:1.)
+; 
+
+
