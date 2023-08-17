1db81d95d0b6a7ef956c4e167f41814b21460ca0
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 09:22:33 -0700

    1b: htdf recipe code-along solution

--- HowToCodeSimple/1b/code_along/01_htdf_recipe.rkt
+++ HowToCodeSimple/1b/code_along/01_htdf_recipe.rkt
@@ -8,3 +8,21 @@
 ; produces twice that number. Call your function double.
 ; 
 
+
+(: double (Number -> Number))
+;; Produce 2 times the given number.
+
+(check-expect (double 0) 0)
+(check-expect (double -1) -2)
+(check-expect (double 2) 4)
+(check-expect (double 3.1) 6.2)
+
+#;
+(define (double n) 0)
+
+#;
+(define (double n)
+  (... n))
+
+(define (double n)
+  (* 2 n))
