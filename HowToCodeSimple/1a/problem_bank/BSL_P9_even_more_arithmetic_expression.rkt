309f8e23582340115e27cd8a5f1b9f725c4f0f8f
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 21:12:33 -0700

    1a: P9 even more arithmetic expression problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P9_even_more_arithmetic_expression.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P9_even_more_arithmetic_expression) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM 
+; 
+; Write the BSL expression that represents the arithmetic expression:     
+; (6 + 3) * (9 - 7)
+; 
