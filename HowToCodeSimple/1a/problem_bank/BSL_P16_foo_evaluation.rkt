c88adcf17dc5beff5957d420325e62acfbe3062d
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 08:51:42 -0700

    1a: P16 foo evaluation solution

--- HowToCodeSimple/1a/problem_bank/BSL_P16_foo_evaluation.rkt
+++ HowToCodeSimple/1a/problem_bank/BSL_P16_foo_evaluation.rkt
@@ -14,3 +14,15 @@
       s))
 
 (foo (substring "abcde" 0 3))
+(foo "abc")
+(if (string=? (substring "abc" 0 1) "a")
+      (string-append "abc" "a")
+      "abc")
+(if (string=? "a" "a")
+      (string-append "abc" "a")
+      "abc")
+(if #true
+      (string-append "abc" "a")
+      "abc")
+(string-append "abc" "a")
+"abca"
