2e46eca7107f54baf08d83bdfdd106bd1405474c
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 08:49:25 -0700

    1a: P16 foo evaluation problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P16_foo_evaluation.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P16_foo_evaluation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write out the step-by-step evaluation of the expression below.
+; 
+
+
+(define (foo s)
+  (if (string=? (substring s 0 1) "a")
+      (string-append s "a")
+      s))
+
+(foo (substring "abcde" 0 3))
