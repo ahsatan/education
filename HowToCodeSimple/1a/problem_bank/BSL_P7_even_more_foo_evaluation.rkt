5422392f8f0155f637dc46f8b59fbe2160108981
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-16 21:03:31 -0700

    1a: P7 even more foo evaluation problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P7_even_more_foo_evaluation.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P7_even_more_foo_evaluation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write out the step-by-step evaluation of the expression below.
+; 
+
+
+(define (farfle s)
+  (string-append s s))
+
+(farfle (substring "abcdef" 0 2))
