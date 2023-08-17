d8998c008c9ae89980692b0ac2f0dff2627dccd0
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 08:45:14 -0700

    1a: P15 function writing problem

---
+++ HowToCodeSimple/1a/problem_bank/BSL_P15_function_writing.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname BSL_P15_function_writing) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write a function that consumes two numbers and produces the larger of the two.
+; 
