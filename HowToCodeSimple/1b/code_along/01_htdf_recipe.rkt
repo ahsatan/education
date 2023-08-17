011188d4289d6a28d45f87d34af63ee060c7a89d
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 09:05:11 -0700

    1b: htdf recipe code-along problem

---
+++ HowToCodeSimple/1b/code_along/01_htdf_recipe.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 01_htdf_recipe) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes a number and
+; produces twice that number. Call your function double.
+; 
+
