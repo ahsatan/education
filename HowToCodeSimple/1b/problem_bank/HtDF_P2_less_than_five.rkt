8e6e2f4f6248d86e366755fc7590042cebf88aa1
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 21:19:17 -0700

    1b: P2 less than five problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P2_less_than_five.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P2_less_than_five) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes a string and
+; determines whether its length is less than 5.
+; 
