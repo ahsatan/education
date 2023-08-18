2f1994c2b5a219b1ed2ff56392caef39f247c97e
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 09:53:13 -0700

    1b: P3 boxify problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P3_boxify.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P3_boxify) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes an image
+; and puts a box around it.
+; 
+; (boxify (ellipse 60 30 "solid" "red")) should produce .
+; 
+
+
+(require 2htdp/image)
