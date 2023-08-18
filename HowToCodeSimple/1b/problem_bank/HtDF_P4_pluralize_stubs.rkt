055049b025edfedacc8c029cd11bef3c788da87d
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 13:34:16 -0700

    1b: P4 pluralize stubs problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P4_pluralize_stubs.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P4_pluralize_stubs) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Write three stubs with different bodies that are consistent with the signature and purpose below.
+; 
+
+
+(: pluralize (String -> String))
+;; Pluralizes given string by appending "s" to the end.
