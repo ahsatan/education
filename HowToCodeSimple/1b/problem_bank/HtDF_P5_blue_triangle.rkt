51fc4bfa4fbae0a775dc9b713b14740729118f4d
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 14:53:27 -0700

    1b: P5 blue triangle solution

--- HowToCodeSimple/1b/problem_bank/HtDF_P5_blue_triangle.rkt
+++ HowToCodeSimple/1b/problem_bank/HtDF_P5_blue_triangle.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P5_blue_triangle) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname HtDF_P5_blue_triangle) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; 
 ; PROBLEM
 ; 
@@ -8,3 +8,21 @@
 ; produces a blue solid triangle of that size.
 ; 
 
+
+(require 2htdp/image)
+(define Image (signature (predicate image?)))
+
+(: blue-triangle (Natural -> Image))
+;; Produce a blue triangle of the given size.
+
+(check-expect (blue-triangle 25) (triangle 25 "solid" "blue"))
+
+#;
+(define (blue-triangle s) empty-image)
+
+#;
+(define (blue-triangle s)
+  (... s))
+
+(define (blue-triangle s)
+  (triangle s "solid" "blue"))
