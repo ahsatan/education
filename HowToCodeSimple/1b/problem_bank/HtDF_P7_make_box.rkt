5a83e0285c8d6b72142f9f0d9824234d04ff2cdb
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 15:03:46 -0700

    1b: P7 make box solution

--- HowToCodeSimple/1b/problem_bank/HtDF_P7_make_box.rkt
+++ HowToCodeSimple/1b/problem_bank/HtDF_P7_make_box.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P7_make_box) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname HtDF_P7_make_box) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; 
 ; PROBLEM
 ; 
@@ -10,3 +10,20 @@
 
 
 (require 2htdp/image)
+(define Image (signature (predicate image?)))
+
+(: make-box (String -> Image))
+;; Produce a 10 x 10 square of the given color.
+
+(check-expect (make-box "red") (square 10 "solid" "red"))
+(check-expect (make-box "gold") (square 10 "solid" "gold"))
+
+#;
+(define (make-box c) empty-image)
+
+#;
+(define (make-box c)
+  (... c))
+
+(define (make-box c)
+  (square 10 "solid" c))
