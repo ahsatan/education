7d22278388d057e93fe0eb44bc2ec7efa7d8b1a0
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 13:32:14 -0700

    1b: P3 boxify solution

--- HowToCodeSimple/1b/problem_bank/HtDF_P3_boxify.rkt
+++ HowToCodeSimple/1b/problem_bank/HtDF_P3_boxify.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P3_boxify) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname HtDF_P3_boxify) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; 
 ; PROBLEM
 ; 
@@ -12,3 +12,22 @@
 
 
 (require 2htdp/image)
+(define Image (signature (predicate image?)))
+
+(: boxify (Image -> Image))
+;; Produce a copy of the given image with an outline box around it.
+
+(check-expect (boxify (ellipse 60 30 "solid" "red"))
+              (overlay (ellipse 60 30 "solid" "red")
+                       (rectangle 62 32 "outline" "black")))
+
+#;
+(define (boxify i) i)
+
+#;
+(define (boxify i)
+  (... i))
+
+(define (boxify i)
+  (overlay i
+           (rectangle (+ 2 (image-width i)) (+ 2 (image-height i)) "outline" "black")))
