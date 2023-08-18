0c47a3e4f59e5090bfe503dafe0a0875bb1d54b5
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 15:06:48 -0700

    1b: P8 ensure question problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P8_ensure_question.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P8_ensure_question) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes a string, and adds "?" 
+; to the end unless the string already ends in "?".  Assume given string length > 0.
+; 
