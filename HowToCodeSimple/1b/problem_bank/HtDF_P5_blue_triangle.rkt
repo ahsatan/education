de4962eb604268dfdde602d0e687dbd417b80a34
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 13:38:31 -0700

    1b: P5 blue triangle problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P5_blue_triangle.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P5_blue_triangle) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes a number and
+; produces a blue solid triangle of that size.
+; 
+
