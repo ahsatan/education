e4e556b07818fd78625c8666754426d45398222f
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-17 21:14:39 -0700

    1b: P1 summon problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P1_summon.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P1_summon) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that generates a summoning charm.
+; 
+;    (summon "Firebolt") should produce "accio Firebolt"
+;    (summon "portkey")  should produce "accio portkey"
+;    (summon "broom")    should produce "accio broom"
+; 
