fc5c73a72a4e7cab22999316662a67063ae2b1cc
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 15:00:24 -0700

    1b: P7 make box problem

---
+++ HowToCodeSimple/1b/problem_bank/HtDF_P7_make_box.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_P7_make_box) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes a color, and creates a
+; solid 10x10 square of that colour.
+; 
+
+
+(require 2htdp/image)
