687789cb5df3955ff6e6e7ffa721dcc23f4989a8
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-19 16:06:41 -0700

    2: CA6 interval solution

--- HowToCodeSimple/2/code_along/06_interval.rkt
+++ HowToCodeSimple/2/code_along/06_interval.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 06_interval) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname 06_interval) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; 
 ; PROBLEM
 ; 
@@ -10,3 +10,22 @@
 ; Design a data definition to represent a seat number in a row, where each
 ; row has 32 seats. (Just the seat number, not the row number.)
 ;  
+
+
+(: 1-32? (Natural -> Boolean))
+(define (1-32? n)
+  (<= 1 n 32))
+
+(define SeatNum (signature (predicate 1-32?)))
+;; interp. Seat number in a theatre row, 1 and 32 are aisle seats.
+
+(: SN1 SeatNum)
+(define SN1 1)
+(: SN2 SeatNum)
+(define SN2 13)
+(: SN3 SeatNum)
+(define SN3 32)
+
+#;
+(define (fn-for-seat-num sn)
+  (... sn))
