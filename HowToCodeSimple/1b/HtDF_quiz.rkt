0b63ee3c80ee564e6aa4c0ca8a584e16d3c71d8e
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-18 18:08:48 -0700

    1b: HtDF quiz

---
+++ HowToCodeSimple/1b/HtDF_quiz.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname HtDF_quiz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Follow the HtDF recipe to design a function that consumes two images and
+; produces #true if the first is larger than the second.
+; 
