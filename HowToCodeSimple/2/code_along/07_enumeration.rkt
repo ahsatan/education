2103abaa6da2204b5296cc00dde092ff7bf589c4
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-19 16:08:26 -0700

    2: CA7 enumeration problem

---
+++ HowToCodeSimple/2/code_along/07_enumeration.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 07_enumeration) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM
+; 
+; Design a data definition to represent the letter grade
+; in a course which is one of A, B, or C.
+;   
