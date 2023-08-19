8cd866fbd46909a384862f2f6b7e8f54a93d6c51
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-19 10:02:15 -0700

    Update signatures

--- HowToCodeSimple/2/code_along/02_data_definitions.rkt
+++ HowToCodeSimple/2/code_along/02_data_definitions.rkt
@@ -2,7 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname 02_data_definitions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; 
-; ;; (: TLColor (enum 0 1 2))
+; (define TLColor (signature (enum 0 1 2)))
 ; ;; interp. 0 means red, 1 yellow, 2 green               
 ; #;
 ; (define (fn-for-tlcolor c)
@@ -10,8 +10,7 @@
 ;         [(= c 1) (...)]
 ;         [(= c 2) (...)]))
 ; 
-; ;; TLColor -> TLColor
-; (: next-color ((enum 0 1 2) -> (enum 0 1 2)))
+; (: next-color (TLColor -> TLColor))
 ; ;; produce next color of traffic light
 ; (check-expect (next-color 0) 2)
 ; (check-expect (next-color 1) 0)
@@ -28,7 +27,7 @@
 
 ;; A small part of a traffic simulation.
 
-;; (: TLColor (enum "red" "yellow" "green"))
+(define TLColor (signature (enum "red" "yellow" "green")))
 ;; interp. traffic light colors
 #;
 (define (fn-for-tlcolor c)
@@ -36,8 +35,7 @@
         [(string=? c "yellow") (...)]
         [(string=? c "green") (...)]))
 
-;; TLColor -> TLColor
-(: next-color ((enum "red" "yellow" "green") -> (enum "red" "yellow" "green")))
+(: next-color (TLColor -> TLColor))
 ;; Produce next color of traffic light.
 
 (check-expect (next-color "red") "green")
