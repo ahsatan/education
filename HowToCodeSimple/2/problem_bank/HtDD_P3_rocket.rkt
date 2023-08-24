0fa00ae92596c59708d20b513f8907f7af1d0f3f
Natasha Lemak <natasha.lemak@gmail.com>
2023-08-23 18:33:18 -0700

    2: P3 rocket problem

---
+++ HowToCodeSimple/2/problem_bank/HtDD_P3_rocket.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDD_P3_rocket) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 
+; PROBLEM A
+; 
+; Design a data definition RocketDescent to represent a rocket's journey as it descends 
+; from 100 kilometers to Earth. Once the rocket has landed it is done.
+; 
+
+
+
+; 
+; PROBLEM B
+; 
+; Design a function rocket-descent-to-msg that will output the rocket's
+; remaining descent distance in a short string that can be broadcast. 
+; When the descent is over, the message should be "The rocket has landed!".
+; 
+
